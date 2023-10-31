9d78e6c31a6232ff51af78a56064c082a9ef7841
Peter K Dunn <pdunn2@usc.edu.au>
2023-11-01 08:20:24 +1000

    Fix errors in documentation

--- R/Snakes.R
+++ R/Snakes.R
@@ -8,11 +8,16 @@
 #'
 #' @format A data frame with 28 rows (each plot) and 4 columns:
 #' \describe{
-#'   \item{Crayfish}{Whether the lizard lives in a crayfish region or not; one of `Y` or `N`}
-#'   \item{Sex}{The lizard sex; one of `male` or `female`}
+#'   \item{Crayfish}{Whether the snake lives in a crayfish region or not; one of `Y` or `N`}
+#'   \item{Sex}{The snake sex; one of `male` or `female`}
 #'   \item{SVL}{The snout-to-length length, in cm}
 #'   \item{Teeth}{The number of number of maxillary teeth}
 #' }
+#' 
+#' Cabrera, Doreen; Griffen, Blaine D. (2023). 
+#' Data for: Northern crayfish form alternation morphometrics [Dataset]. 
+#' Dryad. https://doi.org/10.5061/dryad.15dv41p2j
+#' 
 #' @source Manjarrez, J., Macias Garcia, C., & Drummond, H. (2017). 
 #' Morphological convergence in a Mexican garter snake associated with the ingestion of a novel prey. 
 #' \emph{Ecology and Evolution}, \bold{7}(18), 7178--7186.
