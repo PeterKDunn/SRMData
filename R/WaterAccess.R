c91edae04ac6db7aebf9883f9db38e15ee41ef06
Peter K Dunn <pdunn2@usc.edu.au>
2023-12-01 11:40:55 +1000

    Water data: First pass

---
+++ R/WaterAccess.R
@@ -0,0 +1,29 @@
+#' WaterAccess
+#'
+#' WaterAccess data set
+#' 
+#' @docType data
+#' 
+#' @keywords datasets
+#'
+#' @format A data frame with 150 rows (15 participants by 10 reps each) and 3 columns:
+#' \describe{
+#'   \item{ID}{The particiapnt ID}
+#'   \item{Trial}{Which trial; one of \code{1} to \code{5}}
+#'   \item{Speed}{The average running speed, in km.h}
+#'   \item{HRM}{The vertical oscillation (VO) as measured by the Garmin Heart Rate Monitor-Pro (HRM), in cm}
+#'   \item{NOVA}{The VO as measured by the  the INCUS NOVA device, in cm}
+#'   \item{RDP}{The VO as measured by the Garmin Running Dynamics Pod (RDP), in cm}
+#'   \item{Footpod}{The VO as measured by the Stryd Running Power Meter Footpod (Footpod), in cm}
+#'   \item{Video}{The VO as measured by video analysis, in cm}
+#' }
+#' @source 
+#' Nounkeu, C. D., Metapi, Y. D., Ouabo, F. K., Kamguem, A. S. T., Nono, B., Azza, N., Leumeni, P., Nguefack-{T}sague, G., Todem, D., Dharod, J. M., & Kuate, D. (2022). 
+#' "Assessment of drinking water access and household water insecurity: A cross sectional study in three rural communities of the Menoua division, West Cameroon". 
+#' \emph{PLOS Water}, \bold{1}(8), e0000029.
+#' 
+#' 
+#' @usage data(WaterAccess)
+
+"WaterAccess"
+
