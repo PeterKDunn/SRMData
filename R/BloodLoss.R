72348c9b7264597df96eb580943a8164ca0e80ba
Peter Dunn <pdunn2@usc.edu.au>
2024-02-14 15:41:26 +1000

    Add BloodLoss

---
+++ R/BloodLoss.R
@@ -0,0 +1,23 @@
+#' BloodLoss
+#'
+#' BloodLoss data set
+#' 
+#' @docType data
+#' 
+#' @keywords datasets
+#'
+#' @format A data frame with 36 rows and 4 columns:
+#' \describe{
+#'   \item{High1}{Instrument 1 measurement of luteotropichormone (LH) concentrations at a high level, in mIU/ml}
+#'   \item{Mid1}{Instrument 1 measurement of LH concentrations at a middle level, in mIU/ml}
+#'   \item{High2}{Instrument 2 measurement of LH concentrations at a high level, in mIU/ml}
+#'   \item{Mid2}{Instrument 2 measurement of LH concentrations at a middle level, in mIU/ml}
+#' }
+#' @note The known mean values are, resspectively, 64.31, 19.24, 64.97 and 19.40 mIU/ml.
+#' @source Feng, Yang-chun and Huang, Yan-chun and Ma, Xiu-min. 2017.
+#' The application of {S}tudent's $t$-test in internal quality control of clinical laboratory.
+#' \emph{Frontiers in Laboratory Medicine} \bold{1} (3): 125--128.
+#' 
+#' @usage data(BloodLoss)
+
+"BloodLoss"
