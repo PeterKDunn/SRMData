c516a144cc25da6139da64978f19a35996254cc0
Peter Dunn <pdunn2@usc.edu.au>
2024-06-10 07:43:09 +1000

    Fix typo

--- R/RipsID.R
+++ R/RipsID.R
@@ -11,7 +11,7 @@
 #' \describe{
 #'   \item{AgeGroup}{The age group of the person; one of \code{1} (18 to 24), \code{2} (25 to 34), \code{3} (25 to 50) or \code{4} (51 to 65)}
 #'   \item{Identification}{Whether the person correctly identified a rip from a picture; one of \code{1} (correctly) or \code{2} (incorrectly)}
-#'   \item{Counts}{The number of people in each cel}
+#'   \item{Counts}{The number of people in each cell}
 #' }
 #' 
 #' @details The data provide the number of people correctly identifying a rip from a photo, by age group.
