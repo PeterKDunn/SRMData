6a9187d69c636e7c6cd2681bb516baebd126165b
Peter Dunn <pdunn2@usc.edu.au>
2024-06-25 17:26:44 +1000

    SWMT help

---
+++ R/SixMWT.R
@@ -0,0 +1,22 @@
+#' Six-minute walk time test results
+#'
+#'Six-minute walk time data set
+#' 
+#' @docType data
+#' 
+#' @keywords datasets
+#'
+#' @format A data frame with 50 rows (one per subject) and 3 columns:
+#' \describe{
+#'   \item{Distance20}{The 6MWT distance in a 20m corridor, in m}
+#'   \item{Distance30}{The 6MWT distance in a 30m corridor, in m}
+#'   \item{Age}{The age of the subject, in completed years}
+#' }
+#' @source Saiphoklang, N., Pugongchai, A., & Leelasittikul, K. (2022). 
+#' Comparison between 20 and 30 meters in walkway length affecting the 6-minute walk test in patients with chronic obstructive pulmonary disease: A randomized crossover study. 
+#' \emph{Plos One}, \bold{17}(1), e0262238.
+#' 
+#' @usage data(SixMWT)
+
+"SixMWT"
+
