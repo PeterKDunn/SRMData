b2ff1e4037cf7f1c19a427aa7931387b3365dfca
Peter K Dunn <pdunn2@usc.edu.au>
2024-08-06 09:32:52 +1000

    Fix bizarre entries in Dential (where from...?): `No_duplicated_No` and `Yes_duplicated_Yes`

--- R/Dental.R
+++ R/Dental.R
@@ -10,7 +10,7 @@
 #' @format A data frame with 90 rows (one per person) and 4 columns:
 #' \describe{
 #'   \item{Country}{the country; a factor}
-#'   \item{Indus}{whether the country is considered an industrialized country; a factor with levels \code{Ind} (industrialized) or \code{NonInd} (not industrialized)}
+#'   \item{Indus}{whether the country is considered an industrialized country; a factor with levels \code{Yes} (industrialized) or \code{No} (not industrialized)}
 #'   \item{Sugar}{the mean annual sugar consumption in kilograms per person per year, computed over the five years (or as much as available) prior to the survey; a numeric vector}
 #'   \item{DMFT}{estimates of the mean number of decayed, missing and filled teeth at age 12; a numeric vector}
 #' }
