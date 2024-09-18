affbd4aa79133acf47ce4c6903bad8d229e59ca0
Peter Dunn <pdunn2@usc.edu.au>
2024-09-18 16:56:30 +1000

    Minor edits, fixes

--- R/Jeans.R
+++ R/Jeans.R
@@ -6,10 +6,10 @@
 #' 
 #' @keywords datasets
 #'
-#' @format A data frame with 28 rows (each plot) and 4 columns:
+#' @format A data frame with 80 rows (each pair of jeans) and 14 columns:
 #' \describe{
 #'    \item{Brand}{The brand of jeans; 22 brands are represented}
-#'    \item{Style}{The style of jeans; one of \code{boot-cut}, \code{reular}, \code{skinny}, \code{slim} or \code{straight}}
+#'    \item{Style}{The style of jeans; one of \code{boot-cut}, \code{regular}, \code{skinny}, \code{slim} or \code{straight}}
 #'    \item{Sex}{Whether the jeans are men's or women's jeans; one of \code{men} or \code{women}}
 #'    \item{Price}{The price, in US dollars}
 #'    \item{MaxHeightFront}{The height (in cm) of the front pocket from the top of the highest rivet to the lowest point of the pocket (along the left-hand side or zipper side)} 
@@ -20,6 +20,8 @@
 #'    \item{MinHeightBack}{The height (in cm) from the shallowest point of the back pocket to the top of the pocket} 
 #'    \item{MaxWidthBack}{The width of the pocket at the very top (the opening)} 
 #'    \item{MinWidthBack}{The width of the pocket at its narrowest (just before the pocket tapers to a point)} 
+#'    \item{Area}{The area of the pocket, from the pocket's measurements (in square cm)}
+#'    \item{Style2}{The style, where \code{skinny} now means \code{Style == "skinny" |  "slim"} and where \code{straight} means \code{Style == "straight" | "boot-cut"}}
 #' }
 #' 
 #' @source 
@@ -32,6 +34,7 @@
 #' 
 #' @note
 #' The \code{githib} source contains a diagram explaining the pocket measurements more clearly.
+#' All jeans that were measured have a 32-inch waistband, as indicated by the brand.
 #' 
 #' @usage data(Jeans)
 
