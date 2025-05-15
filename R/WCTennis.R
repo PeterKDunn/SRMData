fbb41b3bd1b785903e3a490bf10f1f5a0a0272c9
Peter K. Dunn <pdunn2@usc.edu.au>
2025-05-15 10:07:53 +1000

    Corrections

--- R/WCTennis.R
+++ R/WCTennis.R
@@ -1,18 +1,17 @@
 #' Wheelchair tennis
 #'
-#' The push-time for wheelchair tennis players, 
-#' with and without holding a racquet.
+#' The push time for wheelchair tennis players, with and without holding a racquet.
 #' 
 #' 
 #' @docType data
 #' 
 #' @keywords datasets
 #'
-#' @format A data frame with 13 rows (each plants) and 3 columns:
+#' @format A data frame with 13 rows (each player) and 3 columns:
 #' \describe{
 #'   \item{Person}{The person}
-#'   \item{PTwith}{The pushtime, when holding a racquet; in seconds}
-#'   \item{PTwithout}{The push-time, without holding a racquet; in seconds}
+#'   \item{PTwith}{The push time, when holding a racquet; in seconds}
+#'   \item{PTwithout}{The push time, without holding a racquet; in seconds}
 #' }
 #' 
 #' @source 
@@ -23,7 +22,7 @@
 #'  https://doi.org/10.17026/dans-xjf-bs8v, 
 #'  \emph{DANS Data Station Life Sciences}, V1.
 #'  
-#'  @references
+#' @references
 #' Alberca, I., Chénier, F., Astier, M., Watelain, E., Vallier, J. M., Pradon, D., & Faupin, A. (2022). 
 #' Sprint performance and force application of tennis players during manual
 #'  wheelchair propulsion with and without holding a tennis racket. 
